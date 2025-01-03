2530ee103b0eeb4054dc91d11a57b267073c9c20
Vikas Chauhan <chauhanvikas734@gmail.com>
2025-01-03 12:37:03 +0530

    pdf_assistant.py running successfully

--- pdf_assistant.py
+++ pdf_assistant.py
@@ -2,7 +2,7 @@ import typer
 from typing import Optional, List
 from phi.assistant import Assistant
 from phi.storage.assistant.postgres import PgAssistantStorage
-from phi.knowledge.pdf import PDFKnowledgeBase
+from phi.knowledge.pdf import PDFUrlKnowledgeBase
 from phi.vectordb.pgvector import PgVector2
 import os
 from dotenv import load_dotenv
@@ -11,7 +11,7 @@ load_dotenv()
 os.environ["GROQ_API_KEY"]=os.getenv("GROQ_API_KEY")
 db_url = "postgresql+psycopg://ai:ai@localhost:5532/ai"
 
-knowledge_base=PDFKnowledgeBase(
+knowledge_base=PDFUrlKnowledgeBase(
     urls=['https://phi-public.s3.amazonaws.com/recipes/ThaiRecipes.pdf'],
     vector_db=PgVector2(collection="recipes",db_url=db_url)
 )
